b52ebfa26484c9ad21af1ba3fd5d345b999f4ba7
Josué Kirchner <josueantoniok@gmail.com>
2022-11-23 14:05:20 -0300

    addTest/NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <NotFound />', () => {
+  it('Se a página contém um heading \'h2\' com o texto \'Page requested not found\'', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const title = getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+
+    expect(title).toBeInTheDocument();
+  });
+  it('Se a página mostra a imagem \'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif\'', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const img = getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
